6fa4c522d7977eae25e95672add749617c18ed6f
Andre Annunziata <andre.annunziata@usil.pe>
2019-11-20 08:36:12 -0500

    Ejercicio3

---
+++ Ejercicio3.py
@@ -0,0 +1,87 @@
+import random
+while True:
+    N = int(input("Ingrese un numero entre 2 y 5: "))
+    if N>=2 and N<=5:
+        break
+    else:
+        print("Intente nuevamente")
+if N==2:
+    for i in range(1):
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        c = random.randint(1,100)
+        d = random.randint(1,100)
+        print(a,b)
+        print(c,d)
+if N==3:
+    for i in range(1):
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        c = random.randint(1,100)
+        d = random.randint(1,100)
+        e = random.randint(1,100)
+        f = random.randint(1,100)
+        g = random.randint(1,100)
+        h = random.randint(1,100)
+        i = random.randint(1,100)
+        print(a,b,c)
+        print(d,e,f)
+        print(g,h,i)        
+if N==4:
+    for i in range(1):
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        c = random.randint(1,100)
+        d = random.randint(1,100)
+        e = random.randint(1,100)
+        f = random.randint(1,100)
+        g = random.randint(1,100)
+        h = random.randint(1,100)
+        i = random.randint(1,100)
+        j = random.randint(1,100)
+        k = random.randint(1,100)
+        l = random.randint(1,100)
+        m = random.randint(1,100)
+        n = random.randint(1,100)
+        o = random.randint(1,100)
+        p = random.randint(1,100)
+        
+        print(a,b,c,d)
+        print(e,f,g,h)
+        print(i,j,k,l) 
+        print(m,n,o,p)
+         
+if N==5:
+    for i in range(1):
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        c = random.randint(1,100)
+        d = random.randint(1,100)
+        e = random.randint(1,100)
+        f = random.randint(1,100)
+        g = random.randint(1,100)
+        h = random.randint(1,100)
+        i = random.randint(1,100)
+        j = random.randint(1,100)
+        k = random.randint(1,100)
+        l = random.randint(1,100)
+        m = random.randint(1,100)
+        n = random.randint(1,100)
+        o = random.randint(1,100)
+        p = random.randint(1,100)
+        q = random.randint(1,100)
+        r = random.randint(1,100)
+        s = random.randint(1,100)
+        t = random.randint(1,100)
+        u = random.randint(1,100)
+        v = random.randint(1,100)
+        w = random.randint(1,100)
+        x = random.randint(1,100)
+        y = random.randint(1,100)
+
+        
+        print(a,b,c,d,e)
+        print(f,g,h,i,j)
+        print(k,l,m,n,o) 
+        print(p,q,r,s,t)
+        print(u,v,w,x,y)   
